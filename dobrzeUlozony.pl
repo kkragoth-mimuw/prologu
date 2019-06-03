bb628789900960c3396e4f668506ccbbbb282b71
Piotr Szulc <kkragoth@abyssus.localdomain>
2019-06-03 19:54:39 +0200

    find end

--- dobrzeUlozony.pl
+++ dobrzeUlozony.pl
@@ -3,5 +3,21 @@
 findEnd(Graph, End) :- findEnds(Graph, [], [End]).
 
 findEnds([], Ends, Ends).
-findEnds([node(V, [], _ ) | L], Ends, Result) :-
-    findEnds(L, [V|Ends], Result).
+findEnds([node(V, [], _ ) | L], Ends, Results) :-
+    findEnds(L, [V|Ends], Results).
+
+findStart(Graph, Start) :- findStarts(Graph, [], [], [], [Start]).
+
+findStarts([], [], _, Starts, Starts).
+
+findStarts([], [V|L], InEdges, Starts, Results) :-
+    \+ member(V, InEdges),
+    findStarts([], L, InEdges, [V|Starts], Results).
+
+findStarts([], [V|L], InEdges, Starts, Results) :-
+    \+ member(V, InEdges),
+    findStarts([], L, InEdges, [Starts], Results).
+
+% findStarts(Graph, Vertices, InEdges, Results).
+findStarts([node(V, E, _) | L], Vertices, InEdges, Starts, Results) :-
+    findStarts(L, [V | Vertices], [E | InEdges], Starts, Results).
