8b4057041ad020edf2f72f64095e39ed9cebdf2c
kkragoth <kkragoth@gmail.com>
2019-06-04 20:17:09 +0200

    fix rewrite

--- efgGraph.pl
+++ efgGraph.pl
@@ -1,21 +1,21 @@
 jestEFGrafem(Graph) :-
     checkDuplicates(Graph, [], Vertices),
-    checkUndeclared(Graph, Vertices).
+    checkUndeclared(Graph, Vertices),
     checkGraphSymmetric(Graph, Graph).
 
 checkDuplicates([], Vertices, Vertices).
-checkDuplicates([node(V, _, _ ) | L , Vertices, Result) :-
+checkDuplicates([node(V, _, _ ) | L], Vertices, Result) :-
     \+ member(V, Vertices),
     checkDuplicates(L, [V | Vertices], Result).
 
 checkUndeclared([], _).
 checkUndeclared([node(_, E, F)|L], Vertices) :-
     listContainedInList(E, Vertices),
-    listContainedInList(F, Vertices)
+    listContainedInList(F, Vertices),
     checkUndeclared(L, Vertices).
 
 checkGraphSymmetric(_, []).
-checkGraphSymmetric(Graph, [node(V, E, F)|L] :-
+checkGraphSymmetric(Graph, [node(V, _E, F)|L]) :-
     checkIfFedgesAreSymmetric(Graph, F, V),
     checkGraphSymmetric(Graph, L).
 
@@ -24,12 +24,11 @@ checkIfFedgesAreSymmetric(Graph, [E|F], V) :-
     checkIfVisInE(Graph, E, V),
     checkIfFedgesAreSymmetric(Graph, F, V).
 
-checkIfVisInE([node(V, A, F) | L], V, Vstart) :-
+checkIfVisInE([node(V, _A, F) | _L], V, Vstart) :-
     member(Vstart, F).
 
-checkIfVisInE([_|L], V, Vstart) :-cesList([E|L], Vertices)
+checkIfVisInE([_|L], V, Vstart) :-
     checkIfVisInE(L, V, Vstart).
-
 
 listContainedInList([], _).
 listContainedInList([E|L], Vertices) :-
