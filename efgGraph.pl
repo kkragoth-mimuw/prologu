860694090467bcfd71c79b66528be958ad1408b5
kkragoth <kkragoth@gmail.com>
2019-06-07 14:24:13 +0200

    comments

--- efgGraph.pl
+++ efgGraph.pl
@@ -1,16 +1,26 @@
+% Piotr Szulc 347277
+
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-%%% Part first - jestEFGrafem
+%%% Part first
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+%jestEFGrafem(+Term)
 jestEFGrafem(Graph) :-
     checkNoVertexIsDuplicated(Graph),
     \+ undeclaredVertexExists(Graph),
-    \+ asymmetricVertexExists(Graph).
+    \+ asymmetricVertexExists(Graph). % F-edges must be symmetric
 
-checkNoVertexIsDuplicated(Graph) :- checkNoVertexIsDuplicated(Graph, []).
+%checkNoVertexIsDuplicated1(+Term)
+checkNoVertexIsDuplicated(Graph) :-
+    checkNoVertexIsDuplicated(Graph, []).
+
+%checkNoVertexIsDuplicated(+Nodes[], +Verices[])
 checkNoVertexIsDuplicated([], _).
 checkNoVertexIsDuplicated([node(V, _, _ ) | L], Vertices) :-
     \+ member(V, Vertices),
     checkNoVertexIsDuplicated(L, [V | Vertices]).
 
+%undeclaredVertexExists(+Nodes[])
 undeclaredVertexExists(Graph) :-
     getGraphVertices(Graph, Vertices),
     member(node(_, E, F), Graph),
@@ -18,7 +28,8 @@ undeclaredVertexExists(Graph) :-
     member(V, Neighbours),
     \+ member(V, Vertices).
 
-asymmetricVertexExists(Graph) :- 
+%asymmetricVertexExists(+Nodes[])
+asymmetricVertexExists(Graph) :-  % Check if exists asymmetric F-edges
     member(node(V, _, F), Graph),
     member(VSymmetric, F),
     member(node(VSymmetric, _, FSymmetric), Graph),
@@ -26,6 +37,8 @@ asymmetricVertexExists(Graph) :-
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %  Part second - jestDobrzeUlozony
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
 jestDobrzeUlozony(EFgraf) :-
     jestEFGrafem(EFgraf),
 
@@ -38,40 +51,55 @@ jestDobrzeUlozony(EFgraf) :-
     
     length(EFgraf, NumberOfGraphVertices),
     maxEDegreeOfGraph(EFgraf, MaxEDegreeOfGraph),
+
+    % MaxSteps := max(Deg_E_Edges(v) for v in Vertices) * length(Vertices)
     MaxSteps is NumberOfGraphVertices * MaxEDegreeOfGraph,
 
     dfsE(EFgraf, Start, Destination, 1, MaxSteps, [Start]).
 
-
+%fDegreeOverLimit(+Node[], +Int)
 fDegreeOverLimit(Graph, Limit) :-
     member(node(_, _, F), Graph),
     length(F, FDegree),
     FDegree > Limit.
 
+% Below I need to calculate whole lists of starts and ends
+% and check if they contain only one element since I'm not using
+% any generators
+
+%findEnd(+Node[], -Term)
 findEnd(Graph, End) :- findEnds(Graph, [], [End]).
+%findEnds(+Node[], +Term[]: Accumulator, -Term[]: Result)
 findEnds([], Ends, Ends).
-findEnds([node(V, [], _ ) | L], Ends, Results) :-
+findEnds([node(V, [], _ ) | L], Ends, Results) :- % no out edges, V is end
     findEnds(L, [V|Ends], Results).
-findEnds([node(_, E, _) | L], Ends, Results) :-
+findEnds([node(_, E, _) | L], Ends, Results) :- % V has out edges, skip
     length(E, Len),
     Len > 0,
     findEnds(L, Ends, Results).
 
+%findStart(+Node[], -Term)
 findStart(Graph, Start) :- findStarts(Graph, [Start]).
+% List vertices not in set of E-edges
+%findStarts(+Node[], -Term[])
 findStarts(Graph, Starts) :-
     inputEEdges(Graph, [], InputEdges),
     getGraphVertices(Graph, Vertices),
     difference(Vertices, InputEdges, [], Starts).
 
+% List all E-edges
+%inputEEdges(+Node[], +Term[]: Accumulator, -Term[]: Result)
 inputEEdges([], InputEdges, InputEdges).
 inputEEdges([node(_, E, _) | L], InputEdges, Result) :-
     append(E, InputEdges, NewInputEdges),
     inputEEdges(L, NewInputEdges, Result).
 
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-%DFS
 
-dfsE(Graph, Destination, Destination, CurrentStep, MaximumSteps, VisitedVerticesSet) :-
+% dfs Over E-edges that has limit of MaximumSteps of how many steps it's allowed to take
+% limit exists in order not to get stuck infinitely on cycle
+%dfsE(+Node[], +Term, +Term, +Int, +Int, +Term[])
+dfsE(Graph, Destination, Destination, CurrentStep, MaximumSteps, VisitedVerticesSet) :- 
+    % We reached destination in limit and we visited all vertices of graph, we're good!
     CurrentStep =< MaximumSteps,
 
     addElementToSet(Destination, VisitedVerticesSet, NewVisitedVerticesSet),
@@ -94,16 +122,20 @@ dfsE(Graph, V, Destination, CurrentStep, MaximumSteps, VisitedVerticesSet) :-
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %  Part three - jestDobrzePermutujacy
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+%jestDobrzePermutujacy(+EFGraf)
 jestDobrzePermutujacy(EFGraf) :-
     jestDobrzeUlozony(EFGraf),
     \+ failsFirstCheck(EFGraf),
-    \+ failsSecondCheck(EFGraf).
+    \+ failsSecondCheck(EFGraf). 
 
 
-failsFirstCheck(Graph) :-
+%failsFirstCheck(+Node[])
+failsFirstCheck(Graph) :- % doesn't pass first implication of task
     findEnd(Graph, End),
 
-    member(node(V, E, F), Graph),
+    member(node(_V, E, F), Graph),
     length(E, ELength), length(F, FLength),
     ELength > 0, 
     FLength > 0,
@@ -121,50 +153,53 @@ failsFirstCheck(Graph) :-
             member(V1, FU)
     ).
 
-failsSecondCheck(Graph) :-
+%failsSecondCheck(+Node[])
+failsSecondCheck(Graph) :-  % doesn't pass second implication of task
     findStart(Graph, Start),
 
-    member(node(V1, E1, F1), Graph),
+    member(node(_V1, E1, F1), Graph),
     member(V, E1),
-    member(node(V, E, F), Graph),
+    member(node(V, _, F), Graph),
     member(W1, F),
 
     W1 \= Start,
 
     \+ (
         member(U, F1),
-        member(node(U, EU, FU, Graph)),
+        member(node(U, EU, _), Graph),
         member(W1, EU)
     ).
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-%  Part fourth - jestSucc - this is the Part where Szubert kills you
+%  Part fourth - jestSucc
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+%jestSucc(+EFGraf, +Term[], +Term[])
 jestSucc(EFGraf, L1, L2) :-
-    \+var(L1),
-    \+var(L2),
+    % According to moodle's forum no need to check here if first argument is EFGraf
     length(L1, M),
     length(L2, N),
     M =< N,
+
     isFPath(EFGraf, L1),
     isFPath(EFGraf, L2),
-    jestSucc3(EFGraf, L1, L2).
+    checkIfIsSuccessor(EFGraf, L1, L2).
 
 
-jestSucc3(_, [], _).
-jestSucc3(EFGraf, [E1|L1], [E2|L2]) :-
+checkIfIsSuccessor(_, [], _).
+checkIfIsSuccessor(EFGraf, [E1|L1], [E2|L2]) :-
     member(node(E1, E, _), EFGraf),
     member(E2, E),
-    jestSucc3(EFGraf, L1, L2).
+    checkIfIsSuccessor(EFGraf, L1, L2).
     
-
-isFPath(EFGraf, []).
-isFPath(EFGraf, [E|L]) :- isFPath(EFGraf, E, L).
-isFPath(EFGraf, _, []).
-isFPath(EFGraf, V, [E|L]) :-
-    member(node(V, _, F), EFGraf),
-    member(E, F),
-    isFPath(EFGraf, E, L).
-
+%isFPath(+Node[], +Term[])
+isFPath(_, []).
+isFPath(EFGraf, [V]) :-
+    member(node(V, _, _), EFGraf).
+isFPath(EFGraf, [V1, V2|L]) :- 
+    member(node(V1, _, F), EFGraf),
+    member(V2, F),
+    isFPath(EFGraf, [V2|L]).
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %% EFGraf utils
